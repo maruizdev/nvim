36c9189c5c4fe85fb30d6dba6b26f9745e551d8f
Marcelo Ruiz <mackciber@gmail.com>
2025-04-06 10:35:03 -0400

    add monokai pro

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,6 +1,2 @@
 -- vim.cmd("colorscheme gruvbox-material")
--- vim.cmd("colorscheme onedark")
--- vim.cmd("colorscheme kanagawa-paper")
--- vim.cmd("colorscheme kanagawa-paper-ink")
-vim.cmd("colorscheme arctic") --vscode
--- vim.cmd("colorscheme hybrid")
+vim.cmd("colorscheme monokai-pro")
--- lua/plugins/hybrid.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"HoNamDuong/hybrid.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-}

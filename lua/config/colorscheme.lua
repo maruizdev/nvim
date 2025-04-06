5e686dd406606bbe9b48fbdbec1d2c56e1dd23a2
Marcelo Ruiz <mackciber@gmail.com>
2025-04-05 20:25:57 -0400

    colorscheme hybrid

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,1 +1,5 @@
-vim.cmd("colorscheme gruvbox-material")
+-- vim.cmd("colorscheme gruvbox-material")
+-- vim.cmd("colorscheme onedark")
+-- vim.cmd("colorscheme kanagawa-paper")
+-- vim.cmd("colorscheme arctic") --vscode
+vim.cmd("colorscheme hybrid")

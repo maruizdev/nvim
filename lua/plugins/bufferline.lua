3e76794e6f8b03239ed558ebd4d1250d797e547a
Marcelo Ruiz <marcelo@mruiz.autoridadminera.gob.bo>
2025-04-25 11:31:30 -0400

    config bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,5 +10,17 @@ return {
 			numbers = "ordinal",
 			separator_style = "padded_slant"
 		}
-	}
+	},
+	config = function()
+		options = {
+			-- No abrir pestañas nuevas, solo buffers
+			separator_style = "slant",   -- o "thick", "thin", "flat" según prefieras
+			always_show_bufferline = true, -- Muestra siempre la barra de buffers
+			show_buffer_close_icons = true,
+			show_close_icon = true,
+			-- Puedes personalizar el comportamiento de abrir un archivo en el buffer actual:
+			-- Cambiar la acción de abrir nuevos buffers a no abrir en una nueva ventana o pestaña
+		}
+	end
+
 }

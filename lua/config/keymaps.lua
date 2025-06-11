6e5674b69f0421fc3e0f6268b373ef47737ff4ee
Marcelo Ruiz <marcelo@mruiz.autoridadminera.gob.bo>
2025-06-11 13:52:52 -0400

    tab bar config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,9 +22,9 @@ vim.keymap.set('n', "<S-Tab>", ":tabprevious<CR>", { noremap = true })
 vim.keymap.set('n', "<S-t>", ":tabnew<CR>", { noremap = true, silent = true }) ]]
 
 -- Navigation commands bufferline
--- vim.keymap.set('n', '<S-TAB>', ':bprevious<CR>', opts)
--- vim.keymap.set('n', '<TAB>', ':bnext<CR>', opts)
--- vim.keymap.set('n',"<leader>q", ":Bdelete<CR>", {noremap =true, silent = true})
+vim.keymap.set('n', '<S-TAB>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<TAB>', ':bnext<CR>', opts)
+vim.keymap.set('n',"<leader>q", ":Bdelete<CR>", {noremap =true, silent = true})
 
 
 --save file
--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,14 +1,33 @@
 return {
-'romgrk/barbar.nvim',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function() vim.g.barbar_auto_setup = false end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+	'kdheepak/tabline.nvim',
+	dependencies = {
+		'nvim-tree/nvim-web-devicons',
+		'hoob3rt/lualine.nvim',
+		'kyazdani42/nvim-web-devicons',
+		'famiu/bufdelete.nvim'
+	},
+	config = function()
+		require 'tabline'.setup {
+			-- Defaults configuration options
+			enable = true,
+			options = {
+				-- If lualine is installed tabline will use separators configured in lualine by default.
+				-- These options can be used to override those settings.
+				section_separators = { '', '' },
+				component_separators = { '', '' },
+				max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+				show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
+				show_devicons = true,    -- this shows devicons in buffer section
+				show_bufnr = false,      -- this appends [bufnr] to buffer section,
+				show_filename_only = true, -- shows base filename only instead of relative path in filename
+				modified_icon = "+ ",    -- change the default modified icon
+				modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+				show_tabs_only = false,  -- this shows only tabs instead of tabs + buffers
+			}
+		}
+		vim.cmd [[
+      set guioptions-=e " Use showtabline in gui vim
+      set sessionoptions+=tabpages,globals " store tabpages and globals in session
+    ]]
+	end,
 }

fca2cf4e6f06c90b9604347a66e768d32d248860
Marcelo Ruiz <marcelo@mruiz.autoridadminera.gob.bo>
2025-06-17 10:27:32 -0400

    bdelte

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,6 +20,7 @@ vim.keymap.set("n", "<C-l>", [[<Cmd>wincmd l<CR>]], { desc = "Move right" })
 vim.keymap.set('n', "<Tab>", ":tabnext<CR>", { noremap = true })
 vim.keymap.set('n', "<S-Tab>", ":tabprevious<CR>", { noremap = true })
 vim.keymap.set('n', "<S-t>", ":tabnew<CR>", { noremap = true, silent = true })
+vim.keymap.set('n',"<leader>q", ":bdelete<CR>", {noremap =true, silent = true})
 
 -- Navigation commands bufferline
 --[[ vim.keymap.set('n', '<S-TAB>', ':bprevious<CR>', opts)
